ffd9118660009450eab2533ef9ea7d0ce52ecdfc
Kenneth Wright <kenneth.wright@ten53.net>
2024-11-02 06:05:25 +0100

    Added perspective point projection

--- src/display.h
+++ src/display.h
@@ -16,6 +16,7 @@ extern SDL_Texture* color_buffer_texture;
 // signatures
 bool  initialize_window(void);
 void draw_grid(void);
+void draw_pixel(int x, int y, uint32_t color);
 void draw_rect(int x, int y, int width, int height, uint32_t color);
 void render_color_buffer(void);
 void clear_color_buffer(uint32_t color);
--- src/main.c
+++ src/main.c
@@ -2,6 +2,16 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include "display.h"
+#include "vector.h"
+
+
+const int N_POINTS = 9 * 9 * 9;
+vec3_t cube_points[N_POINTS];
+vec2_t projected_points[N_POINTS];
+
+vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
+
+float fov_factor = 640;
 
 bool is_running = false;
 
@@ -35,25 +45,59 @@ void process_input(void) {
                 is_running = false;
             break;
     }
+
+    int point_count = 0;
+
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y += 0.25) {
+            for (float z = -1; z <= 1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z };
+                cube_points[point_count++] = new_point;
+            }
+        }
+    }
+}
+
+
+vec2_t project(vec3_t point) {
+    // converts a vect3_t point to a vec2_t point (strips out .z)
+    vec2_t converted_point = {
+        .x = (fov_factor * point.x) / point.z,
+        .y = (fov_factor * point.y) / point.z
+    };
+    return converted_point;
 }
 
 
 void update(void) {
-    // TODO:
+    for (int i = 0; i < N_POINTS; i++) {
+        vec3_t point = cube_points[i];
+
+        // Move points away from camera position
+        point.z -= camera_position.z;
+
+        // project the current point
+        vec2_t current_point = project(point);
+
+        // save current projected 2D vector in the array of projected points
+        projected_points[i] = current_point;
+    }
 }
 
 
 void render(void) {
-    int red = 255;
-    int green = 0;
-    int blue = 0;
-    int opacity = 255;
-    SDL_SetRenderDrawColor(renderer, red, green, blue, opacity);
-    SDL_RenderClear(renderer);
-
     draw_grid();
 
-    draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+    // loop all projected points and render them
+    for (int i = 0; i < N_POINTS; i++) {
+        vec2_t projected_point = projected_points[i];
+        draw_rect(
+                projected_point.x + (window_width / 2),
+                projected_point.y + (window_height / 2),
+                4,
+                4,
+                0xFFFFFF00);
+    }
 
     render_color_buffer();
 

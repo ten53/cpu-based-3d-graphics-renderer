cd3611ccca80e6cf27e3b32cd5712c2c65bef499
Kenneth Wright <kenneth.wright@ten53.net>
2024-11-03 14:46:11 +0100

    Added dynamic array and flexible mesh data structure

--- src/main.c
+++ src/main.c
@@ -4,15 +4,14 @@
 #include "display.h"
 #include "vector.h"
 #include "mesh.h"
+#include "array.h"
 
+// array of triangles to render frame by frame
+triangle_t* triangles_to_render = NULL;
 
-triangle_t triangles_to_render[N_MESH_FACES];
-
+// globals for execution status and game loop
 vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
-vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
-
 float fov_factor = 640;
-
 bool is_running = false;
 int previous_frame_time = 0;
 
@@ -30,6 +29,9 @@ void setup(void) {
             SDL_TEXTUREACCESS_STREAMING,
             window_width,
             window_height);
+
+    // initialize cube mesh
+    load_cube_mesh_data();
 }
 
 
@@ -70,19 +72,23 @@ void update(void) {
 
     previous_frame_time = SDL_GetTicks();
 
-    cube_rotation.x += 0.01;
-    cube_rotation.y += 0.01;
-    cube_rotation.z += 0.01;
+    // init array of triangles to render
+    triangles_to_render = NULL;
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
 
     // loop all triangle faces of our mesh
-    for (int i = 0; i < N_MESH_FACES; i++) {
-        face_t mesh_face = mesh_faces[i];
+    int num_faces = array_length(mesh.faces);
+    for (int i = 0; i < num_faces; i++) {
+        face_t mesh_face = mesh.faces[i];
 
         // current face
         vec3_t face_vertices[3];
-        face_vertices[0] = mesh_vertices[mesh_face.a - 1]; // -1 to compensate for index placement
-        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
-        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
+        face_vertices[0] = mesh.vertices[mesh_face.a - 1]; // -1 to compensate for index placement
+        face_vertices[1] = mesh.vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh.vertices[mesh_face.c - 1];
 
         triangle_t projected_triangle;
 
@@ -90,9 +96,9 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec3_t transformed_vertex = face_vertices[j];
 
-            transformed_vertex = vec3_rotate_x(transformed_vertex, cube_rotation.x);
-            transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
-            transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
+            transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
 
             // translate vertex awau from camera
             transformed_vertex.z -= camera_position.z;
@@ -109,7 +115,7 @@ void update(void) {
         }
 
         // save current projected triangle in the array of triangles to render
-        triangles_to_render[i] = projected_triangle;
+        array_push(triangles_to_render, projected_triangle);
     }
 }
 
@@ -118,7 +124,8 @@ void render(void) {
     draw_grid();
 
     // loop all projected points and draw lines and  render them
-    for (int i = 0; i < N_MESH_FACES; i++) {
+    int num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++) {
         triangle_t triangle = triangles_to_render[i];
 
         // draw vertex points
@@ -137,11 +144,22 @@ void render(void) {
                 0xFFFFFF00);
     }
 
+    // clear array of triangles to render every frame loop
+    array_free(triangles_to_render);
+
     render_color_buffer();
 
     clear_color_buffer(0xFF000000);     // ARGB pixel format
 
     SDL_RenderPresent(renderer);
+}
+
+
+// free all dynamically allocated memory
+void free_resources(void) {
+    free(color_buffer);
+    array_free(mesh.faces);
+    array_free(mesh.vertices);
 }
 
 
@@ -158,6 +176,7 @@ int main(void) {
     }
 
     destroy_window();
+    free_resources();
 
     return 0;
 }
--- src/mesh.h
+++ src/mesh.h
@@ -4,12 +4,20 @@
 #include "vector.h"
 #include "triangle.h"
 
-#define N_MESH_VERTICES 8
-extern vec3_t mesh_vertices[N_MESH_VERTICES];
+#define N_CUBE_VERTICES 8
+#define N_CUBE_FACES (6 * 2) // 6 cube faces, 2 triangles per face
 
-#define N_MESH_FACES (6 * 2) // 6 cube faces, 2 triangles per face
-extern face_t mesh_faces[N_MESH_FACES];
+extern vec3_t cube_vertices[N_CUBE_VERTICES];
+extern face_t cube_faces[N_CUBE_FACES];
 
+typedef struct {
+    vec3_t* vertices;   // dynamic array of vertices
+    face_t* faces;      // dynamic array of faces
+    vec3_t rotation;    // rotation (x, y, z) values -  Euler angles
+} mesh_t;
 
+extern mesh_t mesh;     // global variable
+
+void load_cube_mesh_data(void);
 
 #endif
